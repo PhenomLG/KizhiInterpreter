56971fc544e86cd296e478c2baef7fcc15250668
Svyatoslav <slavaanufriev@gmail.com>
2023-03-05 16:21:31 +0300

    Fixed the bug with number 0

--- KizhiInterpreter/Program.cs
+++ KizhiInterpreter/Program.cs
@@ -26,7 +26,7 @@ public class Interpreter
         switch (com.CommandType)
         {
             case CommandType.Set:
-                _writer.SetVariable(com);
+                _operationResult = _writer.SetVariable(com);
                 break;
 
             case CommandType.Sub:
@@ -65,21 +65,24 @@ public class Variable
 
 public class TextWriter
 {
-    private List<Variable> variables = new();
+    public static List<Variable> variables = new();
 
 
-    public void SetVariable(Command command)
+    public bool SetVariable(Command command)
     {
         if (variables.FirstOrDefault(u => u.Name == command.Variable.Name) is null)
             variables.Add(command.Variable);
         else 
             variables.Where(u => u.Name == command.Variable.Name).Select(u => u.Value = command.Variable.Value);
+        return true;
     }
 
     public bool SubVariable(Command command)
     {
         var variable = variables.FirstOrDefault(u => u.Name == command.Variable.Name);
-        if (variable is null) return false;
+        if (variable is null) 
+            return false;
+
         variable.Value -= command.Variable.Value;
         return true;
     }
@@ -87,15 +90,19 @@ public class TextWriter
     public bool PrintVariable(Command command)
     {
         var variable = variables.FirstOrDefault(u => u.Name == command.Variable.Name);
-        if (variable is null) return false;
-        Console.WriteLine(command.Variable.Value);
+        if (variable is null) 
+            return false;
+
+        Console.WriteLine(variable.Value);
         return true;
     }
 
     public bool RemoveVariable(Command command)
     {
         var variable = variables.FirstOrDefault(u => u.Name == command.Variable.Name);
-        if (variable is null) return false;
+        if (variable is null) 
+            return false;
+
         variables.RemoveAll(u => u.Name == command.Variable.Name);
         return true;
     }
